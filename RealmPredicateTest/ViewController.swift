4a2032b118f4bb1b6a3bbeb8cd37dd9002b2cc49
Ramon Honorio <ramonaqh@gmail.com>
2018-10-24 15:15:37 -0300

    Initial Commit

---
+++ RealmPredicateTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealmPredicateTest
+//
+//  Created by Ramon Honório on 24/10/18.
+//  Copyright © 2018 Ramon Honório. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
